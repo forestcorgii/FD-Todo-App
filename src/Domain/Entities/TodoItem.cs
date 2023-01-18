0eff53f4c8d34b6c538406cefa3da27b9d1177ab
Sean Ivan Fernandez_SW Programmer <seanivanf@gmail.com>
2023-01-18 21:47:19 +0800

    Modified domain entity TodoItem, adding Colour property.

--- src/Domain/Entities/TodoItem.cs
+++ src/Domain/Entities/TodoItem.cs
@@ -12,6 +12,8 @@ public class TodoItem : BaseAuditableEntity
 
     public DateTime? Reminder { get; set; }
 
+    public string? Colour { get; set; }
+
     private bool _done;
     public bool Done
     {
