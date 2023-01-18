6bacdddd5c113d05c9ba696dd001907da3c6f2e3
Sean Ivan Fernandez_SW Programmer <seanivanf@gmail.com>
2023-01-18 21:51:59 +0800

    Modified Command UpdateTodoItemDetail, including the use of colour property while also adding a command validator for checking of the colour value is supported.
    Added a Colour property to TodoItemDto.

---
+++ src/Application/TodoItems/Commands/UpdateTodoItemDetail/UpdateTodoItemDetailCommandValidator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentValidation;
+using Todo_App.Application.TodoItems.Queries.GetTodoItemsWithPagination;
+using Todo_App.Domain.ValueObjects;
+
+namespace Todo_App.Application.TodoItems.Commands.UpdateTodoItemDetail;
+public class UpdateTodoItemDetailCommandValidator : AbstractValidator<UpdateTodoItemDetailCommand>
+{
+    public UpdateTodoItemDetailCommandValidator()
+    {
+        RuleFor(x => x.Colour).Must((x, colourText, context) =>
+        {
+            try
+            {
+                _ = Colour.From(colourText);
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }).WithMessage("Unsupported Color.");
+    }
+}
