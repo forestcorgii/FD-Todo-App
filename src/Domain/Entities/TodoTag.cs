4c5a4aeb0315e1ca645e91a91dbb4ed49db237b2
Sean Ivan Fernandez_SW Programmer <seanivanf@gmail.com>
2023-01-20 01:47:09 +0800

    Created an entiry called TodoTags and implement a many-to-many relationship with Todo Item.

---
+++ src/Domain/Entities/TodoTag.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Todo_App.Domain.Entities;
+public class TodoTag : BaseAuditableEntity
+{
+    public string? Name { get; set; }
+    public IList<TodoItem> Items { get; private set; } = new List<TodoItem>();
+}
