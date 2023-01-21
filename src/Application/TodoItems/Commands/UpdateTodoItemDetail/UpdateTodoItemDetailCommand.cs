95e9f0226a8b72fdd264aab1452e417e3898c7ef
Sean Ivan Fernandez_SW Programmer <seanivanf@gmail.com>
2023-01-22 02:59:49 +0800

    Added a logic for adding new tags and removing unused ones.

--- src/Application/TodoItems/Commands/UpdateTodoItemDetail/UpdateTodoItemDetailCommand.cs
+++ src/Application/TodoItems/Commands/UpdateTodoItemDetail/UpdateTodoItemDetailCommand.cs
@@ -1,4 +1,5 @@
 ﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
 using Todo_App.Application.Common.Exceptions;
 using Todo_App.Application.Common.Interfaces;
 using Todo_App.Domain.Entities;
@@ -17,6 +18,9 @@ public record UpdateTodoItemDetailCommand : IRequest
     public PriorityLevel Priority { get; init; }
 
     public string? Note { get; init; }
+
+    public List<string> Tags { get; set; } = new List<string>();
+
 }
 
 public class UpdateTodoItemDetailCommandHandler : IRequestHandler<UpdateTodoItemDetailCommand>
@@ -39,12 +43,44 @@ public class UpdateTodoItemDetailCommandHandler : IRequestHandler<UpdateTodoItem
         }
 
         entity.ListId = request.ListId;
-        entity.Colour= request.Colour;
+        entity.Colour = request.Colour;
         entity.Priority = request.Priority;
         entity.Note = request.Note;
 
+        entity.Tags = await MapToTagEntities(request.Tags, cancellationToken);
+
+        RemoveUnusedTodoTagEntities(cancellationToken);
+
+
         await _context.SaveChangesAsync(cancellationToken);
 
+        var iitem = await _context.TodoItems
+            .FindAsync(new object[] { request.Id }, cancellationToken);
+
         return Unit.Value;
+    }
+
+
+    private async Task<List<TodoTag>> MapToTagEntities(List<string> rawTags, CancellationToken cancellationToken)
+    {
+        List<TodoTag> tags = new();
+        foreach (string tag in rawTags)
+        {
+            TodoTag? tagEntity = await _context.TodoTags.Where(t => t.Name == tag).FirstOrDefaultAsync(cancellationToken);
+            if (tagEntity == null)
+                tags.Add(new TodoTag(tag));
+            else
+                tags.Add(tagEntity);
+        }
+        return tags;
+    }
+
+    private async void RemoveUnusedTodoTagEntities(CancellationToken cancellationToken)
+    {
+        // Check for unused tags to be deleted
+        List<TodoTag> tags = await _context.TodoTags.Include(t=>t.Items).ToListAsync(cancellationToken);
+        foreach (var tag in tags)
+            if (!tag.Items.Any())
+                _context.TodoTags.Remove(tag);
     }
 }
