f3e26404f150956c65a72023e6f10e4ec02125fd
Sean Ivan Fernandez_SW Programmer <seanivanf@gmail.com>
2023-01-22 03:01:17 +0800

    Added a constraint on tag name's format. Added a custom exception for invalid tag name.

--- src/Domain/Entities/TodoTag.cs
+++ src/Domain/Entities/TodoTag.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Todo_App.Domain.Entities;
@@ -9,4 +10,17 @@ public class TodoTag : BaseAuditableEntity
 {
     public string? Name { get; set; }
     public IList<TodoItem> Items { get; private set; } = new List<TodoItem>();
+
+
+    public TodoTag(string? name)
+    {
+        if (!string.IsNullOrEmpty(name))
+            if (Regex.IsMatch(name, "^[a-zA-Z0-9_/-]+$"))
+                Name = name;
+            else
+                throw new InvalidTodoTagException(name);
+        else
+            throw new NullReferenceException();
+    }
+
 }
---
+++ src/Domain/Exceptions/InvalidTodoTagException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Todo_App.Domain.Exceptions;
+public class InvalidTodoTagException:Exception
+{
+    public InvalidTodoTagException(string value) :base($"An invalid special character was detected in {value}."){ }
+}
