76bec1ef3cd97d56019281e9eeed10769eeb021a
Sean Ivan Fernandez_SW Programmer <seanivanf@gmail.com>
2023-01-23 00:36:06 +0800

    Added a GetTodoTags query to be used in updating the Tag list.

---
+++ src/WebUI/Controllers/TodoTagsController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Todo_App.Application.TodoLists.Queries.GetTodos;
+using Todo_App.Application.TodoTags.Queries.GetTodoTags;
+
+namespace Todo_App.WebUI.Controllers;
+public class TodoTagsController : ApiControllerBase
+{
+    [HttpGet]
+    public async Task<ActionResult<List<TodoTagDto>>> Get()
+    {
+        return await Mediator.Send(new GetTodoTagsQuery());
+    }
+
+}
